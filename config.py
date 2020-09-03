a7d375763463ec9bde8aa4c9d7edf61dd5cd2476
Luigi Francesco Cerfeda <luigi.f.cerfeda@gmail.com>
2020-09-03 11:32:00 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -2,24 +2,21 @@ led_irr = LED0 # pin attached to LED that indicates irrigation need
 fan_pin = D19.PWM # pin attached to fan module
 servomotor_pin = D23.PWM # pin attached to servomotor module
 soil_pin = A7 # pin attached to soil module (it is the D39 on pinmap)
-light_led = LED1 # pin attached to LED that indicates light is over threshold
 button_pin = D5 # pin of button to tag dataset
+
+t_acq = 2000 # measurement every t_acq milliseconds
+t_pub_zdm = 5 # publish every t_acq*t_pub_mast seconds
+t_pub_ub = 15 # publish every t_acq*t_pub_ub seconds
 
 ### CHANGES THESE PARAMETERS
 
 WIFI_SSID = ""
 WIFI_PW = ""
 
-threshold_soil = 2500
-threshold_temp = 30
-threshold_hum = 60
-threshold_light = 500
+threshold_temp = 30 # if temp > threshold then activate fan
+threshold_soil = 2500 # if soil > threshold then activate servo
 
 end_angle = 180 # end angle of servomotor
-
-t_acq = 2000 # measurement every t_acq milliseconds
-t_pub_mast = 5 # publish every t_acq*t_pub_mast seconds
-t_pub_ub = 15 # publish every t_acq*t_pub_ub seconds
 
 # weights = [random(1,10)/random(1,10) for i in range(36)] #36 weights
 weights = [-0.065, 0.231, -0.042, -0.272, -0.01, -0.039, 
--- main.py
+++ main.py
@@ -6,44 +6,20 @@
 ######## IMPORT LIBRARIES ########
 
 import streams
-import mcu # to use low level microcontroller functionalities
 import adc # analog to digital converter
-
 import config # configuration file, where there are the parameters to modify
-
 import pwm # PWM library, to operate the fan
-
 from servo import servo # library to use servomotor
-
 from bosch.bme280 import bme280 # library for digital weather sensor (attached to board)
-from stm.hts221 import hts221 # library for digital internal weather sensor (present into board)
-
 from rohm.bh1750fvi import bh1750fvi # library for ambient light (present into board)
-
 from bsp.drivers import wifi
-
 import json
 from zdm import zdm  # Zerynth Device Manager
-
 from genann import genann # library for AI
 
 ######## END IMPORT LIBRARIES ######## 
 
 ### UTILITY FUNCTIONS
-def servomotor_on():
-    
-    print("Servo ON")
-    MyServo.attach()
-    
-    print("Servo controlled in degrees")
-    sleep(100)
-    MyServo.moveToDegree(0)
-    sleep(1000)
-    MyServo.moveToDegree(config.end_angle)
-    
-def servomotor_off():
-    print("servo OFF")
-    MyServo.detach()
 
 interface = None
 device = None
@@ -56,72 +32,73 @@ def connect_wifi_zdm():
         print("Connecting to wifi...")
         # Connect to wifi
         interface.link(config.WIFI_SSID, interface.WIFI_WPA2, config.WIFI_PW)
+        wifi_status = True
+    except Exception as e:
+        print("No Wifi connection")
+        wifi_status = False
+    try:
         if (device == None):
             # Create a ZDM Device
             device = zdm.Device(jobs_dict=my_jobs)
         # Connect the device to the ZDM
         device.connect()
-        wifi_status = True
     except Exception as e:
-        print("No Wifi connection")
-        wifi_status = False
+        print("No ZDM Connection")
     
+
 def check_connection():
     global wifi_status
     wifi_status = interface.is_linked()
     return wifi_status
 
-def pub_data_MAST():
+def pub_data_ZDM():
     global device
-    print('------ publish data MAST ------')
-    print('    External temperature     :', ext_temp)
-    print('    External humidity        :', ext_hum)
-    print('    Internal temperature     :', int_temp)
-    print('    Internal humidity        :', int_hum)
-    print('    Internal pressure        :', int_pres)
-    print('    Soil                     :', soil)
-    print('    Light                    :', light)
-    print('    Irrigation status        :', irrigation_status)
-    print('    Fan status               :', fan_status)
-    print('    Dataset tag              :', tag_dataset)
-    print('-------------------------------')
-    tag = "MAST"
-    payload = {'external_temp': ext_temp, 'external_humidity': ext_hum, 'internal_temp': int_temp, 'internal_humidity': int_hum, 'internal_pressure': int_pres, 'soil': soil, 'light': light, 'irrigation_status': irrigation_status, "fan_status": fan_status, "tag_dataset": tag_dataset}
-    device.publish(payload, tag)
+    tag = "ZDM"
+    payload = {"temperature": temp, "humidity": hum, "pressure": pres, 'soil': soil, 'light': light, 'irrigation_status': servo_status, "fan_status": fan_status, "tag_dataset": tag_dataset}
+    try: 
+        device.publish(payload, tag)
+        print("--------------------------------------------------------")
+        print('------------- published data with tag ZDM --------------')
+        print("--------------------------------------------------------")
+    except Exception as e:
+        print(e)
+
 
 def pub_data_Ubidots():
     global device
-    print('------ publish data UBIDOTS ------')
-    print('    Soil                     :', soil)
-    print('    Irrigation status        :', irrigation_status)
-    print('-------------------------------')
     tag_ubi = "UBIDOTS"
-    payload_ubi = {'soil': soil, 'irrigation_status': irrigation_status}
-    device.publish(payload_ubi, tag_ubi)
+    payload_ubi = {'soil': soil, 'servo_status': servo_status}
+    try:
+        device.publish(payload_ubi, tag_ubi)
+        print("--------------------------------------------------------")
+        print('------------- published data with tag UBIDOTS ----------')
+        print("--------------------------------------------------------")
+    except Exception as e:
+        print(e)
 
 ### END UTILITY FUNCTIONS
 
 ######## SETUP
+
 
 # create a stream linked to the default serial port
 streams.serial() 
 
 pinMode(config.led_irr, OUTPUT) # configure the irrigation LED
 pinMode(config.fan_pin, OUTPUT) # configure the fan module
-MyServo = servo.Servo(config.servomotor_pin) # configure the servomotor module
-pinMode(config.light_led, OUTPUT) # configure the light LED
 pinMode(config.button_pin, INPUT_PULLUP) # configure the button to tag dataset
+
+MyServo = servo.Servo(config.servomotor_pin) # configure the servomotor module
+MyServo.attach()
 
 try:
     # Setup weather sensor 
     print("start weather sensor...")
-    weather_int = bme280.BME280(I2C1) #SDA1 -> D21; SCL1 -> D22
-    weather_ext = hts221.HTS221(I2C0, D2)
+    weather = bme280.BME280(I2C1) #SDA1 -> D21; SCL1 -> D22
     print("Ready!")
     print("--------------------------------------------------------")
 except Exception as e:
     print("Error: ",e)
-    mcu.reset()
 
 try:
     # Setup ambient light sensor 
@@ -131,8 +108,7 @@ try:
     print("--------------------------------------------------------")
 except Exception as e:
     print("Error: ",e)
-    mcu.reset()
-    
+
 ######## END SETUP
 
 ######## ARTIFICIAL INTELLIGENCE
@@ -182,61 +158,61 @@ connect_wifi_zdm()
 
 try:
     
-    cont_pub_mast = 0
+    cont_pub_zdm = 0
     cont_pub_ub = 0
     
     while True:
         
         try:
-            ext_temp, ext_hum = weather_ext.get_temp_humidity()
-            print("--------------------------------------------------------")
-            print("EXTERNAL VALUES")
-            print("Temperature:", ext_temp, "C")
-            print("Humidity:", ext_hum, "%")
-                
-            int_temp, int_hum, int_pres = weather_int.get_values()
-            print("--------------------------------------------------------")
-            print("INTERNAL VALUES")
-            print("Temperature:", int_temp, "C")
-            print("Humidity:", int_hum, "%")
-            print("Pressure:", int_pres, "hPa")
-                
-            if int_temp > config.threshold_temp:
+            temp, hum, pres = weather.get_values()
+            
+            if temp > config.threshold_temp:
                 fan_status = 1
                 pwm.write(config.fan_pin, 2, 1) # turn on the fan
             else:
                 fan_status = 0
                 pwm.write(config.fan_pin,0,0) # turn off the fan
-            print("fan_status", fan_status)
                 
+            print("--------------------------------------------------------")
+            print("Temperature:", temp, "C")
+            print("Humidity:", hum, "%")
+            print("Pressure:", pres, "hPa")
+            print("Fan Status", fan_status)
             print("--------------------------------------------------------")
         except Exception as e:
             print("weather sensor err", e)
-                
-        tag_dataset = digitalRead(config.button_pin)
-        print("tag_dataset: ", tag_dataset)
         
-        # Acquisition of data from sensors
-        soil = adc.read(config.soil_pin) # soil moisture
-        light = light_sensor.get_value() # ambient light
-        
+        try:
+            soil = adc.read(config.soil_pin) # soil moisture
+            light = light_sensor.get_value() # ambient light
+        except Exception as e:
+            print("light sensor err", e)
+            
         if soil > config.threshold_soil:
-            irrigation_status = 1
+            servo_status = 1
             digitalWrite(config.led_irr, HIGH)
-            servomotor_on()
+            MyServo.moveToDegree(config.end_angle)
+
         else:
-             irrigation_status = 0
-             digitalWrite(config.led_irr, LOW)
-             servomotor_off()
+            servo_status = 0
+            digitalWrite(config.led_irr, LOW)
+            MyServo.moveToDegree(0)
         
-        if (light > config.threshold_light):
-            digitalWrite(config.light_led, HIGH)  # turn the LED ON by setting the voltage HIGH
-        else:
-            digitalWrite(config.light_led, LOW)   # turn the LED OFF by setting the voltage LOW
+        print("--------------------------------------------------------")
+        print("Soil Humidity:", soil)
+        print("Light:", light)
+        print("Servo Status", servo_status)
+        print("--------------------------------------------------------")
+        
+            
+        tag_dataset = digitalRead(config.button_pin)
+        print("--------------------------------------------------------")
+        print("Dataset Tag: ", tag_dataset)
+        print("--------------------------------------------------------")
         
         # NEURAL NETWORK
             
-        '''input_set =  [soil, ext_temp, light, ext_hum, int_pres] # define the inputs
+        '''input_set =  [soil, temp, light, hum, pres] # define the inputs
             
         input_set = [k*1.0 for k in input_set] # ann.run() wants float
     
@@ -247,44 +223,40 @@ try:
         print("Result ANN",out)
         
         if out[0] > 0.45:
-            irrigation_status = 1
+            servo_status = 1
             digitalWrite(config.led_irr, HIGH)
+            MyServo.moveToDegree(config.end_angle)
         else:
-            irrigation_status = 0
-            digitalWrite(config.led_irr, LOW)'''
+            servo_status = 0
+            digitalWrite(config.led_irr, LOW)
+            MyServo.moveToDegree(0)'''
             
-        print("--------------------------------------------------------")
-        print("Soil Humidity:", soil)
-        print("Light:", light)
-        print("irrigation_status", irrigation_status)
-        print("--------------------------------------------------------")
-        
         # publish data on ZDM
-        if (cont_pub_mast >= config.t_pub_mast):
+        if cont_pub_zdm >= config.t_pub_zdm:
             # check status of wifi connection
             if (not check_connection()):
                 connect_wifi_zdm() # if no wifi connection then try to connect
             
             # check again status connection and if ok then publish data
-            if (wifi_status == 1):
+            if wifi_status == 1:
     
                 try:
-                    pub_data_MAST()
+                    pub_data_ZDM()
                 except Exception as e:
-                    print("pub data MAST error: ", e)
+                    print("pub data ZDM error: ", e)
                     
-                cont_pub_mast = 0
+                cont_pub_zdm = 0
         else:
-            cont_pub_mast += 1
+            cont_pub_zdm += 1
         
         # publish data on Ubidots
-        if (cont_pub_ub >= config.t_pub_ub):
+        if cont_pub_ub >= config.t_pub_ub:
             # check status of wifi connection
             if (not check_connection()):
                 connect_wifi_zdm() # if no wifi connection then try to connect
             
             # check again status connection and if ok then publish data
-            if (wifi_status == 1):
+            if wifi_status == 1:
     
                 try:
                     pub_data_Ubidots()
